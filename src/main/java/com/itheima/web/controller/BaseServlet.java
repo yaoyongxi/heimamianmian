ce21939fc6ba8958c3b5d297e8ded57326f24e9b
yaoyongxi <486337849@qq.com>
2026-02-06 11:14:36 +0800

    heimamianmian1.5,部门文件添加成功，成功运行，部门模块有一个500错误

--- src/main/java/com/itheima/web/controller/BaseServlet.java
+++ src/main/java/com/itheima/web/controller/BaseServlet.java
@@ -2,17 +2,19 @@ package com.itheima.web.controller;
 
 import com.itheima.service.store.CompanyService;
 import com.itheima.service.store.impl.CompanyServiceImpl;
+import com.itheima.service.system.DeptService;
+import com.itheima.service.system.impl.DeptServiceImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
 public class BaseServlet extends HttpServlet {
     protected CompanyService companyService;
-//    protected DeptService deptService;
+    protected DeptService deptService;
 
     @Override
     public void init() throws ServletException {
         companyService = new CompanyServiceImpl();
-//        deptService = new DeptServiceImpl();
+        deptService = new DeptServiceImpl();
     }
 }
