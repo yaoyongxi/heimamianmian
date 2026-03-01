537954bff4d7b20ba1f51ba2f0497a512ba99443
yaoyongxi <486337849@qq.com>
2026-03-01 20:34:55 +0800

    Revert "heimamianmian1.8,用户模块完成，成功运行，修改后无乱码，但是新建排序混乱"

--- src/main/java/com/itheima/service/system/impl/UserServiceImpl.java
+++ src/main/java/com/itheima/service/system/impl/UserServiceImpl.java
@@ -6,7 +6,6 @@ import com.itheima.dao.system.UserDao;
 import com.itheima.domain.system.User;
 import com.itheima.factory.MapperFactory;
 import com.itheima.service.system.UserService;
-import com.itheima.utils.MD5Util;
 import com.itheima.utils.TransactionUtil;
 import org.apache.ibatis.session.SqlSession;
 
@@ -25,9 +24,6 @@ public class UserServiceImpl implements UserService {
             //id使用UUID的生成策略来获取
             String id = UUID.randomUUID().toString();
             user.setId(id);
-
-            //密码必须经过加密处理MD5加密
-            user.setPassword(MD5Util.md5(user.getPassword()));
             //3.调用Dao层操作
             userDao.save(user);
             //4.提交事务
@@ -86,10 +82,6 @@ public class UserServiceImpl implements UserService {
             sqlSession = MapperFactory.getSqlSession();
             //2.获取Dao
             UserDao userDao = MapperFactory.getMapper(sqlSession, UserDao.class);
-
-            //方案一：读取现有库中的信息，覆盖现有的数据
-            //方案二：修改update语句
-
             //3.调用Dao层操作
             userDao.update(user);
             //4.提交事务
