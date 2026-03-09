1490597f366726d094bf48e30844ddf7f86e12f2
yaoyongxi <486337849@qq.com>
2026-03-09 19:10:06 +0800

    黑马面面2.6，题目模块基础完成，成功运行

---
+++ src/main/java/com/itheima/web/controller/store/QuestionServlet.java
@@ -0,0 +1,122 @@
+package com.itheima.web.controller.store;
+
+import com.github.pagehelper.PageInfo;
+import com.itheima.domain.store.Catalog;
+import com.itheima.domain.store.Company;
+import com.itheima.domain.store.Course;
+import com.itheima.domain.store.Question;
+import com.itheima.utils.BeanUtil;
+import com.itheima.web.controller.BaseServlet;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+
+//uri:/store/question?operation=list
+@WebServlet("/store/question")
+public class QuestionServlet extends BaseServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String operation = request.getParameter("operation");
+        if("list".equals(operation)){
+            this.list(request, response);
+        } else if ("toAdd".equals(operation)) {
+            this.toAdd(request, response);
+        }else if ("save".equals(operation)) {
+            this.save(request, response);
+        }else if ("toEdit".equals(operation)) {
+            this.toEdit(request,response);
+        }else if ("edit".equals(operation)) {
+            this.edit(request,response);
+        }else if ("delete".equals(operation)) {
+            this.delete(request,response);
+        }
+    }
+
+
+    private void list(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+        //进入列表页
+        //获取数据
+//        QuestionService = new QuestionServiceImpl();
+        int page =1;
+        int size =5;
+        if(StringUtils.isNotBlank(request.getParameter("page"))){
+            page = Integer.parseInt(request.getParameter("page"));
+        }
+        if(StringUtils.isNotBlank(request.getParameter("size"))){
+            page = Integer.parseInt(request.getParameter("size"));
+        }
+        PageInfo all = questionService.findAll(page,size);
+        //将数据保存到指定的位置
+        request.setAttribute("page",all);
+        //跳转页面
+        request.getRequestDispatcher("/WEB-INF/pages/store/question/list.jsp").forward(request,response);
+    }
+    private void toAdd(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+        //加载企业信息
+        List<Company> campanyList = companyService.findAll();
+        request.setAttribute("companyList",campanyList);
+        //加载目录信息
+        List<Catalog> catalogList = catalogService.findAll();
+        request.setAttribute("catalogList",catalogList);
+        //跳转页面
+        request.getRequestDispatcher("/WEB-INF/pages/store/question/add.jsp").forward(request,response);
+    }
+    private void save(HttpServletRequest request,HttpServletResponse response) throws IOException {
+        //将数据获取到，封装成一个对象
+        Question question = BeanUtil.fillBean(request, Question.class,"yyyy-MM-dd");
+        //调用业务层接口Save
+//        QuestionService = new QuestionServiceImpl();
+        questionService.save(question);
+        //跳转回到页面list
+        //list(request, response);
+        response.sendRedirect(request.getContextPath()+"/store/question?operation=list");
+    }
+
+    private void toEdit(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException {
+        //查询要修改的数据findById
+        String id = request.getParameter("id");
+//        QuestionService = new QuestionServiceImpl();
+        Question question = questionService.findById(id);
+        //将数据加载到指定区域，供页面获取
+        request.setAttribute("question",question);
+        //加载企业信息
+        List<Company> campanyList = companyService.findAll();
+        request.setAttribute("companyList",campanyList);
+        //加载目录信息
+        List<Catalog> catalogList = catalogService.findAll();
+        request.setAttribute("catalogList",catalogList);
+        //跳转页面
+        request.getRequestDispatcher("/WEB-INF/pages/store/question/update.jsp").forward(request,response);
+    }
+    private void edit(HttpServletRequest request,HttpServletResponse response) throws IOException {
+        //将数据获取到，封装成一个对象
+        Question question = BeanUtil.fillBean(request, Question.class,"yyyy-MM-dd");
+        //调用业务层接口Save
+//        QuestionService = new QuestionServiceImpl();
+        questionService.update(question);
+        //跳转回到页面list
+        //list(request, response);
+        response.sendRedirect(request.getContextPath()+"/store/question?operation=list");
+    }
+    private void delete(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //将数据获取到，封装成一个对象
+        Question question = BeanUtil.fillBean(request, Question.class);
+        //调用业务层接口Save
+//        QuestionService = new QuestionServiceImpl();
+        questionService.delete(question);
+        //跳转回到页面list
+        //list(request, response);
+        response.sendRedirect(request.getContextPath()+"/store/question?operation=list");
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+}
+
